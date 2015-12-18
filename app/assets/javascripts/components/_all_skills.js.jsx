e350369aa185f7274b0c9f1929c0e509ed0b7f3a
selfup <boudinot.regis@yahoo.com>
2015-12-18 10:08:05 -0700

    clean up - styling

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -2,14 +2,14 @@ var AllSkills = React.createClass({
   getInitialState() {
     return { skills: [] }
   },
-  
+
   componentDidMount() {
-    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
-    console.log('Component Did Mount!');
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) })
+    console.log('Component Did Mount!')
   },
 
   render() {
-    var skills = this.state.skills.map((skill) => {
+    var skills = this.state.skills.map( (skill) => {
       return (
         <div>
           <h3>{skill.name}</h3>
@@ -17,7 +17,7 @@ var AllSkills = React.createClass({
           <p>{skill.details}</p>
         </div>
       )
-    });
+    })
 
     return(
       <div>
