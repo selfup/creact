2987ed111956d62dcffa3b48b3be80ffdc95c32d
selfup <boudinot.regis@yahoo.com>
2015-12-18 10:05:50 -0700

    getting JSON - logging the states

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -1,8 +1,13 @@
 var AllSkills = React.createClass({
+  getInitialState() {
+    return { skills: [] }
+  },
   componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
     console.log('Component Did Mount!');
   },
   render() {
+    console.log(this.state)
     return (
       <div>
         <h1>All of the Skills</h1>
